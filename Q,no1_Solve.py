848c47969a2dbb5a2072eaa190c2b4b016e4a114
JAI PRAKASH KUMAR <67636249+JPK10125@users.noreply.github.com>
2021-11-19 10:42:05 +0530

    Create Q,no1_Solve.py

---
+++ Q,no1_Solve.py
@@ -0,0 +1,11 @@
+#Q,no1 --  You are given a string. Split the string on a " " (space) delimiter and join using a - hyphen.
+def split_and_join(line):
+    x = line.split()
+    y = "-".join(x)
+    return y
+    # write your code here
+
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print(result)
